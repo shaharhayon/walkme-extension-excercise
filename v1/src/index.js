859e122261c938aad37b083bddc5fe82f0e41de0
Shahar Hayon <shahar.hayon@WM-HWVQ7W4YYF.local>
2024-12-31 10:58:53 +0200

    initial commit

---
+++ v1/src/index.js
@@ -0,0 +1,11 @@
+async function sayHello() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+            alert('Hello from the extension!');
+            document.body.style.backgroundColor = 'red';
+        }
+    });
+}
+document.getElementById('myButton').addEventListener('click', sayHello);
