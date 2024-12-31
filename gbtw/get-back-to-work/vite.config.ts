f48b6a2b7999db8ff567830b77b580ee5e85e983
Shahar Hayon <shahar.hayon@WM-HWVQ7W4YYF.local>
2024-12-31 12:19:55 +0200

    add service worker infra

--- gbtw/get-back-to-work/vite.config.ts
+++ gbtw/get-back-to-work/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    emptyOutDir: false,
+    rollupOptions: {
+      input: 'src/service-worker.ts',
+      output: {
+        format: 'esm',
+        entryFileNames: 'service-worker.js',
+      },
+    }
+  }
 })
