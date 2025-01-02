1c23a1639e6b60bf89ad0600b3b8417f20d86580
Shahar Hayon <shahar.hayon@WM-HWVQ7W4YYF.local>
2025-01-02 12:00:19 +0200

    rm facebook gbtw vite project

--- gbtw/get-back-to-work/src/constants.ts
+++
@@ -1,7 +0,0 @@
-const MINUTE = 60;
-export default {
-    TRACKED_URL:'facebook.com',
-    SESSION_TIMEOUT: 5 * MINUTE,
-    SESSION_COOLDOWN: 10 * MINUTE,
-    DAILY_TIMEOUT: 60 * MINUTE
-} as const;
